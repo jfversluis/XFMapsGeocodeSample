b0e245a3462c540ee0bfd1e2c83f95abc7fa9f69
Gerald Versluis <gerald@verslu.is>
2021-06-14 09:30:18 +0200

    Add Maps Sample project

---
+++ XFMapsSample/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace XFMapsSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
