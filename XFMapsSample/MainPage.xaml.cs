21bfc0229a6eaf62f81201a44087a04e17d80b6a
Gerald Versluis <gerald@verslu.is>
2021-06-14 09:50:46 +0200

    Implement Geocoding

--- XFMapsSample/MainPage.xaml.cs
+++ XFMapsSample/MainPage.xaml.cs
@@ -11,9 +11,26 @@ namespace XFMapsSample
 {
     public partial class MainPage : ContentPage
     {
+        private readonly Geocoder _geocoder = new Geocoder();
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void Map_MapClicked(System.Object sender, Xamarin.Forms.Maps.MapClickedEventArgs e)
+        {
+            await DisplayAlert("Coordinates", $"Lat: {e.Position.Latitude}, Long: {e.Position.Longitude}", "OK");
+
+            var addresses = await _geocoder.GetAddressesForPositionAsync(e.Position);
+
+            await DisplayAlert("Address", addresses.FirstOrDefault()?.ToString(), "OK");
+
+            var positions = await _geocoder.GetPositionsForAddressAsync("Broadway, New York");
+
+            await DisplayAlert("Position", $"Lat: {positions.First().Latitude}, Long: {positions.First().Longitude}", "OK");
+
+            myMap.MoveToRegion(MapSpan.FromCenterAndRadius(positions.First(), Distance.FromKilometers(1)));
         }
     }
 }
